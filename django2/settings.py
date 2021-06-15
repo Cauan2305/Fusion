0422ebabdee7e324dab5b31d1814354200c1bcb7
cauan matheus <cauanmatheustrigo68@gmail.com>
2021-06-15 10:59:19 -0300

    commit

--- django2/settings.py
+++ django2/settings.py
@@ -80,28 +80,15 @@ WSGI_APPLICATION = 'django2.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'django2',
-#         'USER': 'geek',
-#         'PASSWORD': 'university',
-#         'HOST': 'localhost',
-#         'PORT': '3306',
-#     }
-# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR , 'db.sqlite3'),
     }
 }
 
 
 
-# DATABASES = {
-#     'default': dj_database_url.config()
-# }
 
 
 
